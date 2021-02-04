7cb8944f5811cdaa68a001b40233ec934bcc6f8e
xieTY <spandelaki@unikadelasalle.ac.id>
2021-02-05 01:59:48 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <p class="text-red-400 font-bold">test warna</p>
+    </div>
   </div>
 </template>
 
